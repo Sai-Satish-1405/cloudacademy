11f1604869095b0d1dfd87a6ef476130abd8be24
Parveen Singh <parveen.singh@cloudacademy.com>
2022-12-13 18:17:48 -0800

    Updaing index

--- pages/index.js
+++ pages/index.js
@@ -6,45 +6,31 @@ export default function Home() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <title>Next.JS with Azure Cosmos DB</title>
-        <meta name="title" content="Next.JS with Azure Cosmos DB" />
+        <title>Next.JS Sample Static Web App</title>
+        <meta name="title" content="Next.JS Sample Static Web App" />
         <meta
           name="description"
-          content="Next JS Application with Azure Cosmos DB Connection"
+          content="Next JS Application"
         />
 
-        <meta property="og:title" content="Next.JS with Azure Cosmos DB" />
+        <meta property="og:title" content="Next.JS Sample Static Web App" />
         <meta
           property="og:description"
-          content="Next JS Application with Azure Cosmos DB Connection"
+          content="Next JS Application"
         />
       </Head>
       <div className="w-full text-center bg-blue-800 flex flex-wrap items-center">
         <div className="text-3xl w-1/2 text-white mx-2 md:mx-auto py-10">
           NextJS - Static Web App Sample
         </div>
-        <div className="w-1/2"></div>
+        <div className="w-1"></div>
       </div>
-      <div className="bg-gray-200 py-10">
-        <div
-          className="flex bg-white shadow-lg rounded-lg mx-2 md:mx-auto mb-10 max-w-2xl"
-        >
+      <div className="bg-gray-200 py-20 min-h-screen">
+        <div className="flex bg-white shadow-lg rounded-lg mx-2 md:mx-auto mb-10 max-w-2xl">
           <div className="flex items-start px-4 py-6">
-            <div className="">
-              <div className="inline items-center justify-between">
-                <h2 className="text-lg font-semibold text-gray-900 -mt-1">
-                </h2>
-                <small className="text-sm text-gray-700 object-right">
-                  Category
-                </small>
-                <small className="ml-3 text-gray-700 text-sm">
-                  Status
-                </small>
-              </div>
-              <p className="mt-3 text-gray-700 text-sm">
-                Description
-              </p>
-            </div>
+              <small className="text-md text-gray-700 object-right">
+                Sample Web Application for Static Web App
+              </small>
           </div>
         </div>
       </div>
